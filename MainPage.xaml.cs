956e4726d8a6527af5bdc575562ad035b9a8fec1
jialinnn <37722330+jialinnn@users.noreply.github.com>
2018-03-29 09:57:07 +1100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aspronto
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            moodPicker.Item.Add(Bad);
+            moodPicker.Item.Add(Okay);
+            moodPicker.Item.Add(Good);
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            dateEntry.Text;
+            painResult.Text;
+            
+            moodPicker.Text;
+            symptomResult.isTogged.ToString();
+
+            dietresult.Text;
+            changeResult.isTogged.ToString();
+
+            additionalComment.Text;
+        }
+
+	}
+}
---
+++ journalPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aspronto
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class journalPage : ContentPage
+	{
+		public journalPage ()
+		{
+			InitializeComponent ();
+		}
+        private void addContent(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+	}
+}
